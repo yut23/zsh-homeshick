0381ef361ee2f7a1b8facee735391e82243e3d9d
yut23 <yut23@gvljohnsons.com>
2015-01-21 11:18:12 -0500

    Various fixes
    * apt-cyg commands are case-insensitive
    * vi-mode prompt cursor works in tmux
    * fixed zsh hanging on launch if X.org had to be started
    * retabbed everything again

--- home/.zsh/Eric-Laptop/env.zsh
+++ home/.zsh/Eric-Laptop/env.zsh
@@ -7,7 +7,7 @@ export UH='/cygdrive/c/Users/Eric'
 
 # Attach to Cygwin/X server if available
 if [ ! -e "/tmp/.X11-unix/X0" ]; then
-	/usr/bin/xlaunch.exe -run /etc/X11/multiwindow.xlaunch >/dev/null 2>&1
+  /usr/bin/cygstart /usr/bin/xlaunch.exe -run /etc/X11/multiwindow.xlaunch >/dev/null 2>&1
 fi
 export DISPLAY=:0.0
 
