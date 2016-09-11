6ca7362e517d4fe29b0967bf1e5a4891ddcd1520
yut23 <yut23@gvljohnsons.com>
2016-09-10 21:36:48 -0400

    Update antigen, add plugins, add take function

--- home/.zsh/antigen.zsh
+++ home/.zsh/antigen.zsh
@@ -7,7 +7,13 @@ antigen use oh-my-zsh
 
 # Bundles from oh-my-zsh repo
 antigen bundle git
+
 antigen bundle zsh-users/zsh-history-substring-search
+# runs ls and git status after cd
+antigen bundle dbkaplun/smart-cd
+# don't automatically run multiline pastes
+antigen bundle oz/safe-paste
+
 
 antigen theme "$HOME/.zsh/themes" yut23
 
---
+++ home/.zsh/functions.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# ~/.zsh/misc.zsh
+
+# lifted from oh-my-zsh
+function take() {
+  mkdir -p $1
+  cd $1
+}
