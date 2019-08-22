a44da15ec1e062d8d66d09b6ef304bdff1c4eac5
yut23 <yut23@gvljohnsons.com>
2019-08-21 21:07:11 -0400

    Fix shared history and create folder if needed

--- home/.zsh/history.zsh
+++ home/.zsh/history.zsh
@@ -1,14 +1,21 @@
 # ~/.zsh/history.zsh
-
 HISTFILE=~/.zsh/history/hist.log
 HISTSIZE=1000000
 SAVEHIST=1000000
-setopt inc_append_history
 setopt hist_ignore_dups
 setopt hist_expire_dups_first
 setopt no_hist_ignore_all_dups
 setopt extended_history
 setopt share_history
+# this is mutually exclusive with share_history
+setopt no_inc_append_history
+setopt no_inc_append_history_time
+setopt hist_fcntl_lock
 
 # prevent history from becoming too cluttered
 HISTORY_IGNORE='(l|l *|ls|ls *|la|la *|ll|ll *|cd|cd ..|cd -*|g st|g ls)'
+
+# ensure history directory exists
+if [[ ! -d ${HISTFILE%/*} ]]; then
+  mkdir -p ${HISTFILE%/*}
+fi
