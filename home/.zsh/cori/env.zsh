f5937d2b2a6539dd05e8571f37ef5ec8351fb185
yut23 <yut23@gvljohnsons.com>
2021-07-26 09:46:35 -0700

    Update code locations on cori

--- home/.zsh/cori/env.zsh
+++ home/.zsh/cori/env.zsh
@@ -1,6 +1,8 @@
 # ~/.zsh/cori/env.zsh
 
-export CASTRO_HOME="$HOME/Castro"
+export CASTRO_HOME="$HOME/dev/Castro"
+export AMREX_HOME="$HOME/dev/amrex"
+export MICROPHYSICS_HOME="$HOME/dev/Microphysics"
 
 module load htop
 module load vim  # this loads a newer version (currently 8.1)
