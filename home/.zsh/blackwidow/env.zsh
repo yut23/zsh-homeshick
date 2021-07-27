90184fe6df41a6823e9298830e2cc8dba77fc04b
yut23 <yut23@gvljohnsons.com>
2021-07-27 19:43:58 -0400

    Move code repos on blackwidow

--- home/.zsh/blackwidow/env.zsh
+++ home/.zsh/blackwidow/env.zsh
@@ -1,5 +1,5 @@
 # ~/.zsh/blackwidow/env.zsh
 
-export CASTRO_HOME="$HOME/research/Castro"
-export AMREX_HOME="$HOME/research/amrex"
-export MICROPHYSICS_HOME="$HOME/research/Microphysics"
+export CASTRO_HOME="$HOME/dev/Castro"
+export AMREX_HOME="$HOME/dev/amrex"
+export MICROPHYSICS_HOME="$HOME/dev/Microphysics"
