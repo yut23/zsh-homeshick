c60abd95bdce6ed01cfa3940ba9a3b7ccf451507
yut23 <yut23@gvljohnsons.com>
2021-08-04 12:04:01 -0400

    Only load modules once

--- home/.zsh/summit/modules.zsh
+++ home/.zsh/summit/modules.zsh
@@ -1,7 +1,4 @@
 # ~/.zsh/summit/modules.zsh
 
-if [[ -z "${MODULES_LOADED+x}" ]]; then
-  # quiet flag for cuda/11.2.0
-  module -q restore development
-  export MODULES_LOADED=1
-fi
+# quiet flag for cuda/11.2.0
+module -q restore development
