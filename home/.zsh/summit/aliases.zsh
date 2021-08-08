a411dfe09e91207d6bb690cf369f17c6c8e691a4
yut23 <yut23@gvljohnsons.com>
2021-08-07 20:41:29 -0400

    Add alias for hsi on Summit

--- home/.zsh/summit/aliases.zsh
+++ home/.zsh/summit/aliases.zsh
@@ -1,6 +1,8 @@
-# ~/.zsh/summit/misc.zsh
+# ~/.zsh/summit/aliases.zsh
 
 # zsh's builtin which is much better
 if [[ $(whence -w which) == 'which: alias' ]]; then
   unalias which
 fi
+
+alias hsi='ptypipe "cd /hpss/prod/$PROJID/users/$USER" hsi'
