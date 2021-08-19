a898fc5b28a78b9c18c1272727a849f7987bc163
yut23 <yut23@gvljohnsons.com>
2021-08-19 17:32:41 -0400

    Update Summit configs

--- home/.zsh/summit/misc.zsh
+++ home/.zsh/summit/misc.zsh
@@ -2,14 +2,14 @@
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/sw/summit/python/3.7/anaconda3/5.3.0/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/sw/summit/python/3.8/anaconda3/2020.07-rhel8/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/sw/summit/python/3.7/anaconda3/5.3.0/etc/profile.d/conda.sh" ]; then
-        . "/sw/summit/python/3.7/anaconda3/5.3.0/etc/profile.d/conda.sh"
+    if [ -f "/sw/summit/python/3.8/anaconda3/2020.07-rhel8/etc/profile.d/conda.sh" ]; then
+        . "/sw/summit/python/3.8/anaconda3/2020.07-rhel8/etc/profile.d/conda.sh"
     else
-        export PATH="/sw/summit/python/3.7/anaconda3/5.3.0/bin:$PATH"
+        export PATH="/sw/summit/python/3.8/anaconda3/2020.07-rhel8/bin:$PATH"
     fi
 fi
 unset __conda_setup
--- home/.zsh/summit/modules.zsh
+++ home/.zsh/summit/modules.zsh
@@ -1,4 +1,3 @@
 # ~/.zsh/summit/modules.zsh
 
-# quiet flag for cuda/11.2.0
-module -q restore development
+module restore development
