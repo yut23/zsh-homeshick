8cab780750e2a589dbc15b4473209181fe3f1e57
yut23 <yut23@gvljohnsons.com>
2021-10-25 16:34:06 -0400

    Add conda config for blackwidow

---
+++ home/.zsh/blackwidow/misc.zsh
@@ -0,0 +1,18 @@
+# ~/.zsh/blackwidow/misc.zsh
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/eric/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/eric/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/eric/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/eric/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate base
