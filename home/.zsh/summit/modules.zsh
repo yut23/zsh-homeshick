1e23d901fdf1f3b8e8427453d198cd36a72ab3a2
yut23 <yut23@gvljohnsons.com>
2021-11-02 21:14:08 -0400

    Automatically remake the Summit module collection if loading fails

--- home/.zsh/summit/modules.zsh
+++ home/.zsh/summit/modules.zsh
@@ -1,3 +1,8 @@
 # ~/.zsh/summit/modules.zsh
 
-module restore development
+if ! module restore development; then
+  if read -q '?Regenerate development collection [yN]? '
+    source ~/.zsh/summit/regen_lmod_collection.zsh
+    # this will leave all the modules loaded
+  fi
+fi
