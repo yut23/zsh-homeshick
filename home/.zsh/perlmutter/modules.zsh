f94bd74fde5861c0e326055e5fd3ac4d94987fae
yut23 <yut23@gvljohnsons.com>
2021-11-09 20:50:34 -0500

    Load all modules manually on Perlmutter

--- home/.zsh/perlmutter/modules.zsh
+++ home/.zsh/perlmutter/modules.zsh
@@ -1,3 +1,6 @@
 # ~/.zsh/perlmutter/modules.zsh
 
-module restore development
+# From a combination of $LMOD_SYSTEM_DEFAULT_MODULES, Default, and the result of
+# `ml PrgEnv-gnu; ml cpu-cuda; ml cuda`.
+module purge
+module load craype-x86-rome craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe-cuda cuda cray-pmi cray-pmi-lib xalt darshan python/3.8-anaconda-2021.05
