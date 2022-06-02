b126595cee55f2b6876386c2d63bf1a43f1c8e3e
yut23 <yut23@gvljohnsons.com>
2022-06-02 16:14:17 -0400

    Update NERSC modules

--- home/.zsh/cori/modules.zsh
+++ home/.zsh/cori/modules.zsh
@@ -1,7 +1,7 @@
 # ~/.zsh/cori/modules.zsh
 
 module load htop
-module load vim  # this loads a newer version (currently 8.1)
-module load tmux/2.8
+#module load vim  # this loads a newer version (currently 8.1)
+#module load tmux/2.8
 
 module swap PrgEnv-intel PrgEnv-gnu
--- home/.zsh/perlmutter/modules.zsh
+++ home/.zsh/perlmutter/modules.zsh
@@ -3,4 +3,4 @@
 # From a combination of $LMOD_SYSTEM_DEFAULT_MODULES, Default, and the result of
 # `ml PrgEnv-gnu; ml cpu-cuda; ml cuda`.
 module purge
-module load craype-x86-rome craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe-cuda cuda cray-pmi cray-pmi-lib xalt darshan python/3.8-anaconda-2021.05
+module load craype-x86-rome craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe-cuda cuda cray-pmi cray-pmi-lib xalt darshan
