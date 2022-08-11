175d59e9d23a29882e9f27b0ef4d9fc8a3b01224
yut23 <yut23@gvljohnsons.com>
2022-08-11 17:34:32 -0400

    Add polaris config

---
+++ home/.zsh/polaris/env.zsh
@@ -0,0 +1,9 @@
+# ~/.zsh/polaris/env.zsh
+
+export CASTRO_HOME="$HOME/dev/Castro"
+export AMREX_HOME="$HOME/dev/amrex"
+export MICROPHYSICS_HOME="$HOME/dev/Microphysics"
+
+export TZ=America/New_York
+
+export PROJID=AstroExplosions
---
+++ home/.zsh/polaris/modules.zsh
@@ -0,0 +1,4 @@
+# ~/.zsh/polaris/modules.zsh
+
+module swap PrgEnv-nvhpc PrgEnv-gnu
+module load nvhpc-mixed
