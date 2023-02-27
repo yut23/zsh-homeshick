5fbe0c62d81fd71e433f17163db22244b3e87a40
yut23 <yut23@gvljohnsons.com>
2023-02-27 15:29:53 -0500

    Set $LMOD_PAGER on xrb

--- home/.zsh/xrb/env.zsh
+++ home/.zsh/xrb/env.zsh
@@ -4,4 +4,7 @@ export CASTRO_HOME="$HOME/dev/Castro"
 export AMREX_HOME="$HOME/dev/amrex"
 export MICROPHYSICS_HOME="$HOME/dev/Microphysics"
 
+# use less instead of more
+export LMOD_PAGER=less
+
 #export SCRATCH=/scratch
