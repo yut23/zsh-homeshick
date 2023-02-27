93c45e82a6bb532ba9a333808b91e6017afadd84
yut23 <yut23@gvljohnsons.com>
2023-02-27 15:34:44 -0500

    Add valgrind to summit modules

--- home/.zsh/summit/regen_lmod_collection.zsh
+++ home/.zsh/summit/regen_lmod_collection.zsh
@@ -1,5 +1,5 @@
 # this file should be sourced, so the module command is available
 
 module purge
-module load gcc/10.2.0 spectrum-mpi lsf-tools hsi darshan-runtime xalt cuda/11.3.1 vim git htop tmux python/3.8-anaconda3 imagemagick
+module load gcc/10.2.0 spectrum-mpi lsf-tools hsi darshan-runtime xalt cuda/11.3.1 vim git htop tmux python/3.8-anaconda3 imagemagick valgrind
 module save development
