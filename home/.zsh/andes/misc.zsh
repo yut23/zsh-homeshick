16d0743f64897229f76e33ad3b10f87deb0e2252
yut23 <yut23@gvljohnsons.com>
2023-08-14 16:02:19 -0400

    Remove andes conda activation from andes/misc.zsh
    It's in plugins.zsh as of c8fbb5a

--- home/.zsh/andes/misc.zsh
+++ home/.zsh/andes/misc.zsh
@@ -1,4 +1,4 @@
 # ~/.zsh/andes/misc.zsh
 
 # use the absolute path to make sure we load the correct environment
-conda activate /ccs/proj/$PROJID/$USER/mambaforge_x86_64/envs/andes_mamba
+#conda activate /ccs/proj/$PROJID/$USER/mambaforge_x86_64/envs/andes_mamba
