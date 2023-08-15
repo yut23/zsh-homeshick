cf9b98a2dc86b554354e8e9f9a883277016778a7
yut23 <yut23@gvljohnsons.com>
2023-08-14 20:07:53 -0400

    Use an anonymous function in perlmutter/modules.zsh

--- home/.zsh/perlmutter/modules.zsh
+++ home/.zsh/perlmutter/modules.zsh
@@ -1,11 +1,15 @@
 # ~/.zsh/perlmutter/modules.zsh
 
-# From a combination of $LMOD_SYSTEM_DEFAULT_MODULES and the result of
-# `ml cudatoolkit`.
-default_modules='craype-x86-milan:craype-network-ofi:perftools-base:xpmem:PrgEnv-gnu:cpe:xalt/2.10.2:gpu:'
+() {
+# notify if the default modules have changed
+local default_modules='craype-x86-milan:craype-network-ofi:perftools-base:xpmem:PrgEnv-gnu:cpe:xalt/2.10.2:gpu:'
 if [[ $LMOD_SYSTEM_DEFAULT_MODULES != $default_modules ]]; then
   echo 'default modules changed, please update ~/.zsh/perlmutter/modules.zsh'
+  # clear the loaded flag so I get nagged inside tmux
+  unset _MODULES_LOADED
 fi
-unset default_modules
+}
 module purge
+# From a combination of $LMOD_SYSTEM_DEFAULT_MODULES and the result of
+# `ml cudatoolkit`.
 module load craype-x86-milan craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe xalt/2.10.2 gpu cudatoolkit
