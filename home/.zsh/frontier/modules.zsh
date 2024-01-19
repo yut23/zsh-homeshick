d0d7c4fcc6451b502bcaec0f77d648c307ce8d1d
yut23 <yut23@gvljohnsons.com>
2024-01-19 17:18:16 -0500

    Update default modules on frontier
    The zsh bug finally got patched (by OLCF staff, not HPE, but oh well)

--- home/.zsh/frontier/modules.zsh
+++ home/.zsh/frontier/modules.zsh
@@ -2,9 +2,10 @@
 
 () {
 # notify if the default modules have changed
-local default_modules='craype-x86-trento craype-network-ofi perftools-base xpmem cray-pmi PrgEnv-cray DefApps :'
+local default_modules='craype-x86-trento craype-network-ofi perftools-base xpmem cray-pmi PrgEnv-cray DefApps'
 if [[ $LMOD_SYSTEM_DEFAULT_MODULES != $default_modules ]]; then
   print -ru2 'default modules changed, please regenerate development collection and update ~/.zsh/frontier/modules.zsh'
+  print -ru2 -f 'new LMOD_SYSTEM_DEFAULT_MODULES=%s\n' "${(q-)LMOD_SYSTEM_DEFAULT_MODULES}"
   # clear the loaded flag so I get nagged inside tmux
   unset _MODULES_LOADED
 fi
