005d2ee250e9c8f9bcc4154fabac90497e2af4cb
yut23 <yut23@gvljohnsons.com>
2024-07-30 16:11:43 -0400

    Update modules on frontier

--- home/.zsh/frontier/regen_lmod_collection.zsh
+++ home/.zsh/frontier/regen_lmod_collection.zsh
@@ -5,7 +5,11 @@ module --no_redirect purge
 # restore the default system modules
 module --no_redirect restore
 # from https://amrex-astro.github.io/workflow/olcf-compilers.html#frontier
-module --no_redirect load PrgEnv-gnu craype-accel-amd-gfx90a cray-mpich rocm/5.3.0 amd-mixed/5.3.0
+module --no_redirect load PrgEnv-gnu
+module --no_redirect load cray-mpich/8.1.28
+module --no_redirect load craype-accel-amd-gfx90a
+module --no_redirect load amd-mixed/6.0.0
+module --no_redirect unload darshan-runtime
 # development tools
 module --no_redirect load git htop tmux imagemagick valgrind4hpc
 
