b4ce081dcc80e50f515836e8e5f733aed269eedb
yut23 <yut23@gvljohnsons.com>
2024-09-02 14:30:42 -0400

    Update perlmutter modules

--- home/.zsh/perlmutter/modules.zsh
+++ home/.zsh/perlmutter/modules.zsh
@@ -2,9 +2,10 @@
 
 () {
 # notify if the default modules have changed
-local default_modules='craype-x86-milan:craype-network-ofi:perftools-base:xpmem:PrgEnv-gnu:cpe:xalt/2.10.2:gpu:'
+local default_modules='craype-x86-milan:craype-network-ofi:perftools-base:xpmem:PrgEnv-gnu:cpe:gpu:'
 if [[ $LMOD_SYSTEM_DEFAULT_MODULES != $default_modules ]]; then
   print -ru2 'default modules changed, please update ~/.zsh/perlmutter/modules.zsh'
+  print -ru2 -f 'new LMOD_SYSTEM_DEFAULT_MODULES=%s\n' "${(q-)LMOD_SYSTEM_DEFAULT_MODULES}"
   # clear the loaded flag so I get nagged inside tmux
   unset _MODULES_LOADED
 fi
@@ -12,4 +13,4 @@ fi
 module --no_redirect purge
 # From a combination of $LMOD_SYSTEM_DEFAULT_MODULES and the result of
 # `ml cudatoolkit`.
-module --no_redirect load craype-x86-milan craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe xalt/2.10.2 gpu cudatoolkit conda
+module --no_redirect load craype-x86-milan craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe gpu cudatoolkit conda
