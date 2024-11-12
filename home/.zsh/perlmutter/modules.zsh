c177cb5df1b7d5706e8089817d6c0dcc8799cb7d
yut23 <yut23@gvljohnsons.com>
2024-11-12 14:42:55 -0500

    Update perlmutter modules

--- home/.zsh/perlmutter/modules.zsh
+++ home/.zsh/perlmutter/modules.zsh
@@ -2,7 +2,7 @@
 
 () {
 # notify if the default modules have changed
-local default_modules='craype-x86-milan:craype-network-ofi:perftools-base:xpmem:PrgEnv-gnu:cpe:gpu:'
+local default_modules='craype-x86-milan:craype-network-ofi:perftools-base:xpmem:PrgEnv-gnu:cpe:gpu:sqs:darshan'
 if [[ $LMOD_SYSTEM_DEFAULT_MODULES != $default_modules ]]; then
   print -ru2 'default modules changed, please update ~/.zsh/perlmutter/modules.zsh'
   print -ru2 -f 'new LMOD_SYSTEM_DEFAULT_MODULES=%s\n' "${(q-)LMOD_SYSTEM_DEFAULT_MODULES}"
@@ -13,4 +13,5 @@ fi
 module --no_redirect purge
 # From a combination of $LMOD_SYSTEM_DEFAULT_MODULES and the result of
 # `ml cudatoolkit`.
-module --no_redirect load craype-x86-milan craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe gpu cudatoolkit
+module --no_redirect load craype-x86-milan craype-network-ofi perftools-base xpmem PrgEnv-gnu cpe gpu darshan cudatoolkit
+module --no_redirect load parallel
