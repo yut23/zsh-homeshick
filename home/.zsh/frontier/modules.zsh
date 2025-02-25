ddc0f0590beac3a17d91e9ac4d16366646f6f33e
yut23 <yut23@gvljohnsons.com>
2025-02-25 15:40:06 -0500

    Update frontier modules to use ROCm 6.3.1

--- home/.zsh/frontier/modules.zsh
+++ home/.zsh/frontier/modules.zsh
@@ -1,19 +1,29 @@
 # ~/.zsh/frontier/modules.zsh
 
-() {
-# notify if the default modules have changed
-local default_modules='craype-x86-trento craype-network-ofi perftools-base xpmem cray-pmi PrgEnv-cray DefApps'
-if [[ $LMOD_SYSTEM_DEFAULT_MODULES != $default_modules ]]; then
-  print -ru2 'default modules changed, please regenerate development collection and update ~/.zsh/frontier/modules.zsh'
-  print -ru2 -f 'new LMOD_SYSTEM_DEFAULT_MODULES=%s\n' "${(q-)LMOD_SYSTEM_DEFAULT_MODULES}"
-  # clear the loaded flag so I get nagged inside tmux
-  unset _MODULES_LOADED
-fi
-}
-if ! module restore development; then
-  if read -q '?Regenerate development collection [yN]? '; then
-    print -ru2
+# update 2025-02-25: something about cray-mpich breaks when loading from a
+# collection, so just load all the modules manually
+#() {
+## notify if the default modules have changed
+#local default_modules='craype-x86-trento craype-network-ofi perftools-base xpmem cray-pmi PrgEnv-cray DefApps'
+#if [[ $LMOD_SYSTEM_DEFAULT_MODULES != $default_modules ]]; then
+#  print -ru2 'default modules changed, please regenerate development collection and update ~/.zsh/frontier/modules.zsh'
+#  print -ru2 -f 'new LMOD_SYSTEM_DEFAULT_MODULES=%s\n' "${(q-)LMOD_SYSTEM_DEFAULT_MODULES}"
+#  # clear the loaded flag so I get nagged inside tmux
+#  unset _MODULES_LOADED
+#fi
+#}
+#if ! module restore development; then
+#  if read -q '?Regenerate development collection [yN]? '; then
+#    print -ru2
     source ~/.zsh/frontier/regen_lmod_collection.zsh
     # this will leave all the modules loaded
-  fi
+#  fi
+#fi
+
+amd_mixed_ver=$(module --redirect --terse list amd-mixed)
+amd_mixed_ver=${amd_mixed_ver#amd-mixed/}
+if [[ -n $amd_mixed_ver ]] && { printf '%s\n' 6.2 "$amd_mixed_ver" | sort --check=quiet --version-sort }; then
+  # workaround needed for amd-mixed/6.2.0 and newer
+  # see https://github.com/AMReX-Astro/workflow/pull/42
+  export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
 fi
--- home/.zsh/frontier/regen_lmod_collection.zsh
+++ home/.zsh/frontier/regen_lmod_collection.zsh
@@ -1,16 +1,17 @@
 # ~/.zsh/frontier/regen_lmod_collection.zsh
 # this file should be sourced, so the module command is available
 
-module --no_redirect purge
+#module --no_redirect purge
 # restore the default system modules
-module --no_redirect restore
+#module --no_redirect restore
 # from https://amrex-astro.github.io/workflow/olcf-compilers.html#frontier
+module --no_redirect load cpe/24.07
 module --no_redirect load PrgEnv-gnu
-module --no_redirect load cray-mpich/8.1.28
+module --no_redirect load cray-mpich/8.1.30
 module --no_redirect load craype-accel-amd-gfx90a
-module --no_redirect load amd-mixed/6.0.0
+module --no_redirect load amd-mixed/6.3.1
 module --no_redirect unload darshan-runtime
 # development tools
 module --no_redirect load git htop tmux imagemagick valgrind4hpc
 
-module --no_redirect save development
+#module --no_redirect save development-6-3-1
